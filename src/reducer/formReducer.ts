f3893126b755b8babc52d366a77e5827600d0ce7
Mary <marieshix98@gmail.com>
2023-02-01 12:03:48 +0300

    Add useReducer to handle form input values

--- src/reducer/formReducer.ts
+++ src/reducer/formReducer.ts
@@ -1,17 +1,26 @@
 import { ActionTypes,initialStateType } from "../model/types";
+interface IProps {
+    key:string;
+    value:string
+}
 interface Props {
     type:string;
-    payload:string;
-}
-
-
-export const formReducer = (state:initialStateType,action:Props) =>{
-    const {type, payload} = action
-    switch(type){
-        case ActionTypes.AddName:
-            return{...state, name:state.name}
-            default:
-                throw new Error(`Unknown action type ${type}`)
+    payload: {
+        key:string;
+        value:string
     }
-
 }
+
+
+export const formReducer = (state:any, action:Props):Props => {
+    const {type, payload} = action
+    switch (type) {
+      case ActionTypes.textInput:
+        return {
+          ...state,
+          [payload.key]: payload.value,
+        };
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+  };
--- src/reducer/index.ts
+++
@@ -1,7 +0,0 @@
-import { useReducer } from "react";
-import { formReducer } from "./formReducer";
-const initialState = {
-    name:'',
-    email: ''
-}
-const [state, dispatch]:any = useReducer<any>(formReducer, initialState)
