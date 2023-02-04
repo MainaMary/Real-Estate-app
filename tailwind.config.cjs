6329c573795814084072ff0a37a377e3ed8a5100
Mary <marieshix98@gmail.com>
2023-02-04 23:54:24 +0300

    Configure global css styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",}
+    },
   },
   plugins: [],
 }
