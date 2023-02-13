bf6b9a0b4ecbecc1cc17e47d779c265f2962893a
Mary <marieshix98@gmail.com>
2023-02-13 15:42:56 +0300

    Clean up app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import { allRoutes } from "./routes/routes";
+import { routes } from "./routes/routes";
 import CustomLoader from "./components/CustomLoader";
 import { RouterProvider } from "react-router-dom";
 function App() {
   return (
     <div className="max-w-[1300px] m-auto">
-      <RouterProvider router={allRoutes} fallbackElement={<CustomLoader />} />
+      <RouterProvider router={routes} fallbackElement={<CustomLoader />} />
     </div>
   );
 }
