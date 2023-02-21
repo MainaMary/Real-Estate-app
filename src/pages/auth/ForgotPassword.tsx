bbeabd762d0dd6289faedbd5ed5f5b222e9e6fd5
Mary <marieshix98@gmail.com>
2023-02-21 23:59:11 +0300

    Forgot passowrd page

---
+++ src/pages/auth/ForgotPassword.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CustomButton from '../../components/CustomButton'
+import CustomLabel from '../../components/CustomLabel'
+import CustomInput from '../../components/CustomInput'
+
+const ForgotPassword = () => {
+  return (
+    <form>
+         <div className="my-4">
+          <CustomLabel>Name</CustomLabel>
+          <CustomInput placeholder="John Doe" name="name" onChange={handleInputChange} type="text" value={name || ''}/>
+          <p className="text-red-500">{formErrors.name}</p>
+        </div>
+    </form>
+  )
+}
+
+export default ForgotPassword
