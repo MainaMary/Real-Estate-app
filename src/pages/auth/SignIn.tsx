fe6c9d20e85c9b32f035a25db2c5ec171568e65e
Mary <marieshix98@gmail.com>
2023-02-23 23:50:56 +0300

    Dispatch login action type when a user signs in

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,75 +1,95 @@
-import React,{useReducer, useState} from 'react'
-import { Link } from 'react-router-dom'
-import { auth } from '../../firebase/firebase'
-import {AiFillEye, AiFillEyeInvisible} from "react-icons/ai"
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { formReducer } from '../../reducer/formReducer'
-import { ActionTypes } from '../../model/types'
-import CustomLabel from '../../components/CustomLabel'
-import CustomInput from '../../components/CustomInput'
-import CustomButton from '../../components/CustomButton'
-import useCounterHook from '../../hooks/useVisiblehook'
-
-
+import React, { useReducer, useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { formReducer } from "../../reducer/formReducer";
+import { ActionTypes } from "../../model/types";
+import CustomLabel from "../../components/CustomLabel";
+import CustomInput from "../../components/CustomInput";
+import CustomButton from "../../components/CustomButton";
+import useCounterHook from "../../hooks/useVisiblehook";
+import { useAuthContext } from "../../context/authContext";
 
 const SignIn = () => {
+  const { dispatchUser } = useAuthContext();
   const initialState = {
-  
-    email: '',
-    password:''
-}
-  const [state, dispatch]:any = useReducer<any>(formReducer, initialState)
-  const {visible, handleVisisble} = useCounterHook()
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error,setError] = useState<string>('')
-  const handleInputChange= (event:any)=>{
-    const {name, value} = event.target
+    email: "",
+    password: "",
+  };
+  const [state, dispatch]: any = useReducer<any>(formReducer, initialState);
+  const { visible, handleVisisble } = useCounterHook();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
     dispatch({
       type: ActionTypes.textInput,
       payload: { key: [name], value: value },
-    })
-  }
-  const {email, password} = state
-  const handleSubmit = async (e:any)=>{
-    e.preventDefault()
-    if( !email || !password){
-      setError('Please provide all details')
+    });
+  };
+  const { email, password } = state;
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Please provide all details");
     }
     try {
       console.log(email, password);
-      if ( password && email) {
-       const res = await signInWithEmailAndPassword(auth, email, password);
-       console.log(res,'response')
-       // navigate("/login");
+      if (password && email) {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        dispatchUser({
+          type: ActionTypes.login,
+          payload: userCredential?.user,
+        });
+
+        // navigate("/login");
       }
 
       setLoading(true);
       setError("");
+    } catch (error: any) {
+      console.log(error.message);
     }
-   catch(error:any){
-    console.log(error.message)
-   }
-  }
+  };
   return (
     <div>
-      <form className='w-full' onSubmit={handleSubmit}>
-        <div className='my-4'>
+      <form className="w-full" onSubmit={handleSubmit}>
+        <div className="my-4">
           <CustomLabel>Email</CustomLabel>
-          <CustomInput placeholder="John Doe" name="name" onChange={handleInputChange} type="text" value={email || ''}/>
+          <CustomInput
+            placeholder="John Doe"
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            value={email || ""}
+          />
         </div>
         <div className="my-4">
           <CustomLabel>Password</CustomLabel>
           <div className="relative">
-          <CustomInput name="password" onChange={handleInputChange} type={visible ? "text" :"password"} value={password || ''}/>
+            <CustomInput
+              name="password"
+              onChange={handleInputChange}
+              type={visible ? "text" : "password"}
+              value={password || ""}
+            />
             <div onClick={handleVisisble} className="absolute right-2 top-3">
-            {visible ? <AiFillEyeInvisible/> : <AiFillEye/>}
+              {visible ? <AiFillEyeInvisible /> : <AiFillEye />}
             </div>
-          
           </div>
-          
         </div>
         <div className="my-4 block  md:flex justify-between">
-          <p>Don't have an account? <span className="text-primary-color"><Link to="/register">Sign up</Link></span></p>
+          <p>
+            Don't have an account?{" "}
+            <span className="text-primary-color">
+              <Link to="/register">Sign up</Link>
+            </span>
+          </p>
           <p>Forgot password?</p>
         </div>
         <div className="my-4">
@@ -77,7 +97,7 @@ const SignIn = () => {
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
